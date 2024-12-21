9e5f21aa79e89af707a8c7b8541cf0837d6118f8
felipelorenzo <lorenzofelipe7@gmail.com>
2024-12-20 22:39:59 -0300

    Agregando comentarios a la query

--- solucion.sql
+++ solucion.sql
@@ -1,32 +1,30 @@
--- 1. Cantidad de pines a comprar
 
-SET @current_day = '2023-09-23'; -- Suponemos que hoy es esa fecha para tener un analisis mas acertado
-SET @stock_seguridad = 20; -- Cantidad de pines
-SET @demora_reposicion = 3; -- Dias
+SET @stock_seguridad = 20; -- Cantidad de pines como stock de seguridad
+SET @demora_reposicion = 3; -- Dias de demora en reposicion
 
-WITH products AS    
-(
-	SELECT DISTINCT   
+WITH products AS
+(	
+	SELECT DISTINCT  -- Tabla de soporte para tener id y description de productos
 		product_id,
         product_description
 	FROM maestro_compras
 ),
 purchases AS   
 (
-	SELECT 
+	SELECT -- Tabla para calcular cantidad de unidades compradas por producto
 		product_id, 
 		product_description, 
-		SUM(quantity_units) AS q_units_purchased
+		SUM(quantity_units) AS q_units_purchased -- Suma de cantidad de unidades de cada lote de compra
 	FROM maestro_compras
 	GROUP BY 1,2
 	ORDER BY 3 DESC
 ),
 sales AS 
 (
-	SELECT 
+	SELECT --  Tabla para calcular cantidad de unidades vendidas por producto
 		MV.product_id, 
         P.product_description, 
-		COUNT(*) AS q_sales
+		COUNT(*) AS q_sales -- Contador de ventas (transaccional)
 	FROM maestro_ventas AS MV
     INNER JOIN products AS P
 		ON MV.product_id = P.product_id
@@ -35,25 +33,25 @@ sales AS
 ),
 stock AS    
 (
-	SELECT 
+	SELECT -- Tabla para calcular el stock actual por producto
 		P.product_description, 
         P.product_id,
-		IFNULL(P.q_units_purchased,0) - IFNULL(S.q_sales,0) AS stock 
+		IFNULL(P.q_units_purchased,0) - IFNULL(S.q_sales,0) AS stock -- Q unidades compradas menos las ventas
 	FROM purchases AS P  
 	LEFT JOIN sales AS S   
 	    ON P.product_id = S.product_id
 ),
 daily_sales AS    
 (
-	SELECT 
+	SELECT -- Tabla para calcular la cantidad diaria promedio de ventas por producto
 		A.product_id,
-        CEIL(AVG(A.q_sales)) AS avg_daily_sales
+        CEIL(AVG(A.q_sales)) AS avg_daily_sales -- Se calcula promedio diario y se redondea al entero superior
 	FROM    
     (
 		SELECT
 			DATE(created_date) AS created_date,
 			product_id,
-			COUNT(*) AS q_sales	
+			COUNT(*) AS q_sales	-- Cantidad de ventas agrupadas por dia
 		FROM maestro_ventas
 		GROUP BY 1,2
     ) AS A
@@ -61,36 +59,39 @@ daily_sales AS
 ),
 cost_revenue AS    
 (
-	SELECT 
+	SELECT -- Tabla para calcular la cantidad diaria promedio de ventas por producto
 		A.product_id, 
 		A.product_description, 
-		ROUND(SUM(amount_unit * quantity_units) / SUM(quantity_units),2) AS avg_unit_cost, -- Precio Promedio Ponderado
+		ROUND(SUM(amount_unit * quantity_units) / SUM(quantity_units),2) AS avg_unit_cost, -- Costo unitario calculado bajo Precio Promedio Ponderado
         unit_sale_price
 	FROM maestro_compras AS A
     LEFT JOIN 
     (
-		SELECT DISTINCT amount AS unit_sale_price, product_id FROM maestro_ventas
+		SELECT DISTINCT 
+			amount AS unit_sale_price, -- Precio de venta por cada producto
+			product_id 
+		FROM maestro_ventas
     ) AS B
     ON A.product_id = B.product_id
 	GROUP BY 1,2,4
 ),
 pre_analisis AS    
 (
-	SELECT 
+	SELECT  -- Tabla para hacer calculos de stock optimo, compras a realizar hoy y dias hasta quebranto
 		S.product_id,
 		S.product_description,
 		S.stock,
 		B.avg_daily_sales,
 		estimated_aggregated_sales,
-		ROUND(CASE WHEN B.avg_daily_sales = 0 THEN NULL ELSE S.stock / B.avg_daily_sales END, 2) AS days_until_stocks_out,
+		ROUND(CASE WHEN B.avg_daily_sales = 0 THEN NULL ELSE S.stock / B.avg_daily_sales END, 2) AS days_until_stocks_out, -- Cuantos días me dura el stock actual con el nivel de ventas promedio
 		CASE 
 			WHEN B.avg_daily_sales = 0 
 			THEN 0
 			WHEN ROUND(S.stock / B.avg_daily_sales, 2) > 3
 			THEN 0
 		ELSE @demora_reposicion - ROUND(S.stock / B.avg_daily_sales, 2) 
-		END AS projected_days_without_stock,
-		B.estimated_aggregated_sales + @stock_seguridad AS optimal_stock,
+		END AS projected_days_without_stock, -- Cuantos dias voy a estar sin stock 
+		B.estimated_aggregated_sales + @stock_seguridad AS optimal_stock, -- Stock optimo
 		CASE 
 			WHEN B.estimated_aggregated_sales = 0 -- Si las ventas estimadas de los proximos 3 dias son 0
 				AND S.stock < @stock_seguridad -- El stock actual es menor al stock de seguridad
@@ -101,21 +102,21 @@ pre_analisis AS
 			WHEN S.stock >= (B.estimated_aggregated_sales + @stock_seguridad) -- Si el stock me alcanza para cubrir las ventas de 3 días + el stock de seguridad
 			THEN 0 -- No compro nada
 		ELSE B.estimated_aggregated_sales + @stock_seguridad - S.stock -- Si el stock se me va a agotar, compro las ventas de 3 dias menos el actual asi renuevo el ciclo
-		END AS today_purchase
+		END AS today_purchase -- Compra de hoy
 	FROM stock AS S
 	LEFT JOIN    
 	(
 		SELECT	
 			product_id,
 			IFNULL(avg_daily_sales,0) AS avg_daily_sales,
-			IFNULL(avg_daily_sales * @demora_reposicion,0) AS estimated_aggregated_sales
+			IFNULL(avg_daily_sales * @demora_reposicion,0) AS estimated_aggregated_sales -- Ventas proyectadas para 3 días
 		FROM daily_sales
 	) AS B
 		ON S.product_id = B.product_id
 ),
 analisis AS    
 (
-	SELECT 
+	SELECT -- Tabla para realizar calculos de Q a devolver, valor de stock actual e ingreso neto post devolucion
 		*,
 		stock * avg_unit_cost AS current_stock_value ,
 		ROUND(((unit_sale_price - avg_unit_cost) * 0.2) * (stock - today_refund),2) AS net_income 
@@ -124,9 +125,9 @@ analisis AS
 			SELECT
 				A.*,
 				CASE 
-					WHEN today_purchase > 0
+					WHEN today_purchase > 0 -- Si tuve que comprar hoy, no devuelvo nada
 					THEN 0
-				ELSE stock - estimated_aggregated_sales - @stock_seguridad
+				ELSE stock - estimated_aggregated_sales - @stock_seguridad -- Si no compre hoy, devuelvo el stock que supere mi stock optimo
 				END AS today_refund,
 				B.avg_unit_cost,
 				B.unit_sale_price
